442a0932e9e3a86562a9cc00e232ceeff783901e
Asel <asel.alimbaeva.kk@gmail.com>
2022-11-12 20:51:51 +0300

    feat: add codes

--- react-todo-list-redux/src/redux/store/index.ts
+++ react-todo-list-redux/src/redux/store/index.ts
@@ -1,8 +1,17 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { doneSlice } from "../slice/done";
+import { inProgressSlice } from "../slice/inProgress";
+import { todoSlice } from "../slice/todo";
 
 export const store = configureStore({
-    reducer: combineReducers({}),
-});
+    reducer: combineReducers({
+      done: doneSlice.reducer,
+      inProgress: inProgressSlice.reducer,
+      todo: todoSlice.reducer,
+    }),
+  });
+  
+  export type StoreDispatch = typeof store.dispatch;
+  export type StoreState = ReturnType<typeof store.getState>;
 
-export type StoreDispatch = typeof store.dispatch;
-export type StoreState = ReturnType<typeof store.getState>;
+// хранит ваши редукторы и объединяет их для использования в нашем приложении
